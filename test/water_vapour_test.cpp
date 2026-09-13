560ee8ab8112853537c847781cf59068c754dc32
Kirikus <kirikusan@gmail.com>
2026-09-14 00:42:55 +0300

    cmake: add new target for tests

---
+++ test/water_vapour_test.cpp
@@ -0,0 +1,13 @@
+#include "../lib/water_vapour.h"
+
+#include <QApplication>
+
+#include "qcustomplot.h"
+
+#if !defined(WIN32)
+#define BOOST_TEST_DYN_LINK
+#endif
+#include <boost/test/unit_test.hpp>
+
+namespace tt = boost::test_tools;
+namespace utf = boost::unit_test;
